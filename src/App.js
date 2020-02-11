fbc51a58bd1f211a072c6f95a7fd98223faef102
Adam Marciniak <adam668@gmail.com>
2020-02-11 11:11:33 -0800

    changed some things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Draggable from 'react-draggable';
+import React, { useRef, useEffect, useState, useCallback } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Draggable from 'react-draggable'
+import findLoops from './getLoops'
+import getGraph from './getGraph'
 
-const Pivots = (props) => {
-
+const Pivots = props => {
   const handleDrag1 = (e, data) => {
     const [x, y] = [data.x + 10, data.y + 10]
     props.setPivotPositions({ ...props.pivotPositions, x1: x, y1: y })
@@ -17,47 +18,59 @@ const Pivots = (props) => {
 
   return (
     <div>
-      <Draggable defaultPosition={{ x: props.startPosition.x1 - 10, y: props.startPosition.y1 - 10 }}
+      <Draggable
+        defaultPosition={{
+          x: props.startPosition.x1 - 10,
+          y: props.startPosition.y1 - 10,
+        }}
         onDrag={handleDrag1}
-        grid={[20, 20]}>
-        <div className='pivot' ref={props.pivotRef1}
-        >{`1 ${props.id}`}</div>
+        grid={[20, 20]}
+      >
+        <div className="pivot" ref={props.pivotRef1}>{`1 ${props.id}`}</div>
       </Draggable>
 
-      <Draggable defaultPosition={{ x: props.startPosition.x2 - 10, y: props.startPosition.y2 - 10 }}
+      <Draggable
+        defaultPosition={{
+          x: props.startPosition.x2 - 10,
+          y: props.startPosition.y2 - 10,
+        }}
         onDrag={handleDrag2}
-        grid={[20, 20]}>
-        <div className='pivot' ref={props.pivotRef2}
-        >2</div>
+        grid={[20, 20]}
+      >
+        <div className="pivot" ref={props.pivotRef2}>
+          2
+        </div>
       </Draggable>
     </div>
-  );
+  )
 }
 
-const Line = (props) => {
+const Line = props => {
   return (
     <svg>
-      <line className='pipeLine'
+      <line
+        className="pipeLine"
         x1={`${props.pivotPositions.x1}`}
         y1={`${props.pivotPositions.y1}`}
         x2={`${props.pivotPositions.x2}`}
-        y2={`${props.pivotPositions.y2}`} />
+        y2={`${props.pivotPositions.y2}`}
+      />
     </svg>
-  );
+  )
 }
 
-const Pipe = (props) => {
+const Pipe = props => {
   const startPosition = { x1: 400, y1: 400, x2: 500, y2: 500 }
-  const [pivotPositions, setPivotPositions] = useState(startPosition);
-  const pivotRef1 = useRef(null);
-  const pivotRef2 = useRef(null);
+  const [pivotPositions, setPivotPositions] = useState(startPosition)
+  const pivotRef1 = useRef(null)
+  const pivotRef2 = useRef(null)
 
   useEffect(() => {
-    props.changePipe(props.id, pivotPositions);
+    props.changePipe(props.id, pivotPositions)
   }, [pivotPositions])
 
   return (
-    <div className='pipe'>
+    <div className="pipe">
       <Pivots
         setPivotPositions={setPivotPositions}
         pivotRef1={pivotRef1}
@@ -69,163 +82,182 @@ const Pipe = (props) => {
       />
       <Line pivotPositions={pivotPositions} />
     </div>
-  );
+  )
+}
+
+function areConnectionsEqual(a, b) {
+  if (a === b) return true
+  if (a == null || b == null) return false
+  if (a.length !== b.length) return false
+  for (var i = 0; i < a.length; ++i) {
+    for (var j = 0; j < a[i].length; ++j) {
+      if (a[i][j] !== b[i][j]) return false
+    }
+  }
+  return true
 }
 
 function App() {
+  const [pipes, setPipes] = useState([
+    { id: 0, x1: null, y1: null, x2: null, y2: null },
+  ])
 
-  const [pipes, setPipes] = useState([{ id: 0, x1: null, y1: null, x2: null, y2: null, node1: 0, node2: 1 }])
+  const [connections, setConnections] = useState([])
 
-  const [connections, setConnections] = useState([]);
+  const [groups, setGroups] = useState([])
 
+  const [loops, setLoops] = useState([])
+
+  const [polygons, setPolygons] = useState([])
 
   const addPipe = () => {
     const getNewId = () => {
       if (pipes.length > 0) {
-        return pipes[pipes.length - 1].id + 1;
+        return pipes[pipes.length - 1].id + 1
       } else {
-        return 0;
+        return 0
       }
-    };
-    const maxNode = 0;
-    pipes.forEach((pipe) => {
-      if (pipe.node1 > maxNode) {
-        maxNode = pipe.node1;
-      }
-      if (pipe.node2 > maxNode) {
-        maxNode = pipe.node2;
-      }
-    })
+    }
 
-    setPipes([...pipes, { id: getNewId(), x1: null, y1: null, x2: null, y2: null, node1: maxNode + 1, node2: maxNode + 2 }])
-  };
+    setPipes([
+      ...pipes,
+      { id: getNewId(), x1: null, y1: null, x2: null, y2: null },
+    ])
+  }
 
   const changePipe = (id, pivots) => {
-    const newPipes = pipes.filter((pipe) => pipe['id'] !== id);
-    setPipes([...newPipes, { 'id': id, ...pivots }])
-  };
+    const newPipes = pipes.filter(pipe => pipe['id'] !== id)
+    setPipes([...newPipes, { id: id, ...pivots }])
+  }
 
   useEffect(() => {
-
-    const connections = [];
-    let idx = 0;
-    pipes.forEach((pipe) => {
-      pipes.slice(idx).forEach((otherPipe) => {
-
+    const newConnections = []
+    let idx = 0
+    pipes.forEach(pipe => {
+      pipes.slice(idx).forEach(otherPipe => {
         if (pipe.id !== otherPipe.id) {
           if (pipe.x1 === otherPipe.x1 && pipe.y1 === otherPipe.y1) {
-            connections.push({ ids: [pipe.id, otherPipe.id], connectors: [1, 1] })
+            newConnections.push({
+              ids: [pipe.id, otherPipe.id],
+              connectors: [1, 1],
+            })
           }
 
           if (pipe.x2 === otherPipe.x2 && pipe.y2 === otherPipe.y2) {
-            connections.push({ ids: [pipe.id, otherPipe.id], connectors: [2, 2] })
+            newConnections.push({
+              ids: [pipe.id, otherPipe.id],
+              connectors: [2, 2],
+            })
           }
 
           if (pipe.x1 === otherPipe.x2 && pipe.y1 === otherPipe.y2) {
-            connections.push({ ids: [pipe.id, otherPipe.id], connectors: [1, 2] })
+            newConnections.push({
+              ids: [pipe.id, otherPipe.id],
+              connectors: [1, 2],
+            })
           }
 
           if (pipe.x2 === otherPipe.x1 && pipe.y2 === otherPipe.y1) {
-            connections.push({ ids: [pipe.id, otherPipe.id], connectors: [2, 1] })
+            newConnections.push({
+              ids: [pipe.id, otherPipe.id],
+              connectors: [2, 1],
+            })
           }
-
         }
       })
-      idx += 1;
+      idx += 1
     })
-    setConnections(connections);
-    console.log('connections', JSON.stringify(connections));
 
-    console.log(pipes);
+    if (
+      !areConnectionsEqual(newConnections, connections) &&
+      newConnections.length !== 0
+    ) {
+      setConnections(newConnections)
+    }
+  }, [pipes, setConnections, connections])
 
-  }, [pipes])
+  const triggerFind = () => {
+    const newPipes = []
+    pipes.forEach(pipe => {
+      newPipes.push({
+        id: pipe.id,
+        point1: { id: 0, coord: [pipe.x1, pipe.y1], group: null },
+        point2: { id: 0, coord: [pipe.x2, pipe.y2], group: null },
+      })
+    })
+    const graphs = getGraph(newPipes)
+    console.log(JSON.stringify(newPipes))
+    const loops = findLoops(graphs.graph)
+    setLoops(loops)
+    setGroups(graphs.coords)
+  }
 
+  useEffect(() => {
+    const polygons = []
+    loops.forEach(loop => {
+      const polygon = []
+      loop.forEach(node => {
+        groups.forEach(group => {
+          if (group[0] === node) {
+            polygon.push(group[1])
+          }
+        })
+      })
+      polygons.push(polygon)
+    })
+    setPolygons(polygons)
+  }, [loops, groups])
 
   return (
-    <div className="App" >
+    <div className="App">
       <div className="toolbar">
-        <button className="toolbarButton" onClick={addPipe}>+</button>
-        <button className="toolbarButton">
-          <svg width="11" height="13" viewBox="0 0 11 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.4972 6.50046L0.337715 12.3661L0.337715 0.634859L10.4972 6.50046Z" fill="#4A4A4A" />
+        <button className="toolbarButton" onClick={addPipe}>
+          +
+        </button>
+        <button className="toolbarButton" onClick={e => triggerFind()}>
+          <svg width="11" height="13" viewBox="0 0 11 13" fill="none">
+            <path
+              d="M10.4972 6.50046L0.337715 12.3661L0.337715 0.634859L10.4972 6.50046Z"
+              fill="#4A4A4A"
+            />
           </svg>
         </button>
       </div>
 
       <div className="workspace">
-        {pipes.map((pipe) => {
+        {pipes.map(pipe => {
           return <Pipe key={pipe.id} id={pipe.id} changePipe={changePipe} />
+        })}
+
+        <svg>
+          {polygons.map(poly => {
+            let polyString = ''
+            poly.forEach(coord => {
+              polyString += `${coord[0]},${coord[1]} `
+            })
+
+            return <polygon key={Math.random()} points={polyString} />
+          })}
+        </svg>
+
+        {groups.map(group => {
+          return (
+            <div
+              key={'group' + group[0]}
+              className="group"
+              style={{
+                top: `${group[1][1] - 20}px`,
+                left: `${group[1][0] - 20}px`,
+              }}
+            />
+          )
         })}
         <div className="gridLines" />
       </div>
 
-      <div className="infoPanel">
-        <div>
-          Pipe ID and Endpoints
-        </div>
-        <ul>
-          {pipes.sort(function (a, b) {
-            return a.id - b.id
-          }).map((pipe) => {
-            return <PipeRow pipe={pipe} key={pipe.id} id={pipe.id} />
-          })}
-        </ul>
-        <div>
-          Connections
-        </div>
-        <ul>
-          {connections.sort(function (a, b) {
-            return a.ids[0] - b.ids[0]
-          }).map((connection) => {
-            return <ConnectionRow key={connection.ids} connection={connection} />
-          })}
-        </ul>
-
-      </div>
+      <div className="infoPanel"></div>
     </div>
-  );
+  )
 }
 
-
-const PipeRow = (props) => {
-  return (
-    <li className="pipeRow">
-      <div>
-        {props.id}
-      </div>
-      <div>
-        x1-
-        {props.pipe.x1}
-      </div>
-      <div>
-        y1-
-        {props.pipe.y1}
-      </div>
-      <div>
-        x2-
-        {props.pipe.x2}
-      </div>
-      <div>
-        y2-
-        {props.pipe.y2}
-      </div>
-    </li>
-  )
-};
-
-const ConnectionRow = (props) => {
-  return (
-    <li className="pipeRow">
-      <div>
-        {props.connection.ids}
-      </div>
-      <div>
-        {props.connection.connectors}
-      </div>
-
-    </li>
-  )
-};
-
-
-export default App;
+export default App
---
+++ src/getGraph.js
@@ -0,0 +1,117 @@
+// const pipes = [
+//   {id: 0,point1:{id: 0,coord:[100,100], group: null},point2:{id: 0,coord:[100,200], group: null}},
+//   {id: 1,point1:{id: 0,coord:[100,100], group: null},point2:{id: 0,coord:[200,100], group: null}},
+//   {id: 2,point1:{id: 0,coord:[100,200], group: null},point2:{id: 0,coord:[200,100], group: null}},
+//   {id: 3,point1:{id: 0,coord:[100,100], group: null},point2:{id: 0,coord:[50,50], group: null}},
+// ]
+
+const getGraph = pipes => {
+  const isSamePoint = (point1, point2) => {
+    if (point1.length !== point2.length || point1.length > 2) {
+      throw new Error('Point array length is not matching or not 2')
+    }
+    if (point1[0] !== point2[0] || point1[1] !== point2[1]) {
+      return false
+    }
+    return true
+  }
+
+  const groups = []
+  const coordsList = []
+
+  const containsCoords = (coordsList, coords) => {
+    for (let i = 0; i < coordsList.length; i += 1) {
+      if (isSamePoint(coordsList[i][1], coords)) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  pipes.forEach(pipe1 => {
+    pipes.forEach(pipe2 => {
+      if (pipe1.id !== pipe2.id) {
+        if (isSamePoint(pipe1.point1.coord, pipe2.point1.coord)) {
+          if (!containsCoords(coordsList, pipe1.point1.coord)) {
+            coordsList.push([groups.length, pipe1.point1.coord])
+            pipe1.point1.group = groups.length
+            pipe2.point1.group = groups.length
+            groups.push(groups.length)
+          } else {
+            const group = coordsList.filter(coord =>
+              isSamePoint(coord[1], pipe1.point1.coord),
+            )[0][0]
+            pipe1.point1.group = group
+            pipe2.point1.group = group
+          }
+        }
+
+        if (isSamePoint(pipe1.point2.coord, pipe2.point2.coord)) {
+          if (!containsCoords(coordsList, pipe1.point2.coord)) {
+            coordsList.push([groups.length, pipe1.point2.coord])
+            pipe1.point2.group = groups.length
+            pipe2.point2.group = groups.length
+            groups.push(groups.length)
+          } else {
+            const group = coordsList.filter(coord =>
+              isSamePoint(coord[1], pipe1.point2.coord),
+            )[0][0]
+            pipe1.point2.group = group
+            pipe2.point2.group = group
+          }
+        }
+
+        if (isSamePoint(pipe1.point1.coord, pipe2.point2.coord)) {
+          if (!containsCoords(coordsList, pipe1.point1.coord)) {
+            coordsList.push([groups.length, pipe1.point1.coord])
+            pipe1.point1.group = groups.length
+            pipe2.point2.group = groups.length
+            groups.push(groups.length)
+          } else {
+            const group = coordsList.filter(coord =>
+              isSamePoint(coord[1], pipe1.point1.coord),
+            )[0][0]
+            pipe1.point1.group = group
+            pipe2.point2.group = group
+          }
+        }
+
+        if (isSamePoint(pipe1.point2.coord, pipe2.point1.coord)) {
+          if (!containsCoords(coordsList, pipe1.point2.coord)) {
+            coordsList.push([groups.length, pipe1.point2.coord])
+            pipe1.point2.group = groups.length
+            pipe2.point1.group = groups.length
+            groups.push(groups.length)
+          } else {
+            const group = coordsList.filter(coord =>
+              isSamePoint(coord[1], pipe1.point2.coord),
+            )[0][0]
+            pipe1.point2.group = group
+            pipe2.point1.group = group
+          }
+        }
+      }
+    })
+  })
+
+  pipes.forEach(pipe => {
+    if (pipe.point1.group === null) {
+      pipe.point1.group = groups.length
+      groups.push(groups.length)
+    }
+    if (pipe.point2.group === null) {
+      pipe.point2.group = groups.length
+      groups.push(groups.length)
+    }
+  })
+
+  const graph = []
+  pipes.forEach(pipe => {
+    graph.push([pipe.point1.group, pipe.point2.group])
+  })
+
+  return { graph: graph, coords: coordsList }
+}
+
+export default getGraph
